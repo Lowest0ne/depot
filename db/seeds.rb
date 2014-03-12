452d82cf874634624a0d9dbc32adc18905bc9663
Carl Schwope <schwope.carl@gmail.com>
2014-03-11 22:51:15 -0400

    functional products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+
+Product.create( title: 'Chicken Egg',
+          description: 'Farm Fresh Eggs.  These eggs will make you <em>live</em>.',
+            image_url: '/images/chicken_egg.jpg',
+                price: 0.25
+)
